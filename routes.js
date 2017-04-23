005cb3d458677395556fbd50c708c67b17c10180
SamA21 <Samaallen@gmail.com>
2017-04-23 12:12:40 +0100

    added titles and config html

--- routes.js
+++ routes.js
@@ -4,15 +4,24 @@ Router.configure({
 
 Router.route('/', {
     name: 'Home',
-    template: 'HomePage'
+    template: 'HomePage',
+    onAfterAction: function() {
+        document.title = 'Budget Home';
+    }
 });
 
 Router.route('/config', {
     name: 'Config',
-    template: 'Config'
+    template: 'Config',
+    onAfterAction: function() {
+        document.title = 'Budget Config';
+    }
 });
 
 Router.route('/budget', {
     name: 'Budget',
-    template: 'Budget'
+    template: 'Budget',
+    onAfterAction: function() {
+        document.title = 'See Your Budget';
+    }
 });
